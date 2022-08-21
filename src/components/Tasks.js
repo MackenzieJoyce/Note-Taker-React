8845da54eac9cac0b7cac2fbe9c9dc8b59783ecc
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-21 15:05:35 -0400

    brought in tasks from global view in app.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,12 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Start Traversy video on React',
-    day: 'Aug 21st at 2:00pm',
-    reminder: true
-  },
-  {
-    id: 2,
-    text: 'Finalize Task Tracker project',
-    day: 'Aug 21st at 4:00pm',
-    reminder: true
-  }
-];
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {    
   return (
     <>
       {/* .map to create a list */}
       {/* Each task is named task */}
       {/* Set the element (h3) and give the information you want to see */}
       {tasks.map((task) => (
-        <h3>{task.text}</h3>
+        <h3 key={task.id}>{task.text}</h3>
       ))}
       ;
     </>
