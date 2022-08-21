b203e562efe6464706a0c5e5d4555bcf198e9c4e
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-21 19:12:03 -0400

    export footer and about. correct spelling on react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { browserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Tasks from './components/Tasks';
