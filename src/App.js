77940860b83e32ca7763382896e61ae616635d71
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-21 14:54:55 -0400

    moved array to be globally reached. imposted useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useState } from 'react';
+
 import Header from './components/Header';
 import Tasks from './components/Tasks'
 
 // Component using JSX
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Start Traversy video on React',
+      day: 'Aug 21st at 2:00pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Finalize Task Tracker project',
+      day: 'Aug 21st at 4:00pm',
+      reminder: true
+    }
+  ]);
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 };
