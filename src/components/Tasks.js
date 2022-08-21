b4d4acaff12da7170faba52762a9b7a9d4d2cc88
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-21 18:50:51 -0400

    removed unnecessary text

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -5,12 +5,10 @@ const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {/* .map to loop through the array */}
-      {/*  */}
           {tasks.map((task) => (
         //   Task component and passing task in as a prop
               <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
       ))}
-      ;
     </>
   );
 };
