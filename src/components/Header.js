a6763e3f670cac5f21f1b139d1cc3afd31a1bca1
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-21 19:19:25 -0400

    made a condition saying if the pathname is '/' then you'll see the button, otherwise you won't

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
 import Button from './Button';
 
 const Header = ({ title, onAdd, showAdd }) => {
+const location = useLocation() 
+
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+      {/* Putting this in {} allows the button to be part of a condition */}
+      {/* Rather than using a turnary, this is saying if the path you're on is '/' then you will also be seeing this Button */}
+      {/* Else is implied, saying if its anything other than '/' don't show the button */}
+      {/* The button is only shown if the path name is true */}
+      {location.pathname === '/' && <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />}
     </header>
   );
 };
